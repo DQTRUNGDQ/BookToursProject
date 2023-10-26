b0c917b250a9bd0c7ac199d0ede923c897b28ef2
DQTRUNGDQ <trungthpthy@gmail.com>
2023-10-26 10:54:28 +0700

    Update: Agreegation Pipeline, Document MiddelWare

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 // Create Model
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -19,6 +21,10 @@ const tourSchema = new mongoose.Schema({
     ratingsAverage:{
         type: Number,
         default: 4.5
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
     },
     ratingsQuantity: {
         type: Number,
@@ -49,7 +55,31 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: { virtuals: true},
+    toObject: {virtuals: true}
 });
+
+tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7;
+})
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function(next) {
+//     console.log('Will save document...')
+//     next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc);
+//     next();
+// })
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
